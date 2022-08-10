4f89c0e34f8cb3c80ec0defb3b21ca0f3f42ce55
Ran Ren <ranforrestren@gmail.com>
2022-08-10 17:07:34 -0400

    Add logic for allowing players to mark tiles (no visuals yet)

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,40 @@
 // object responsible for handling board state
 const gameBoard = (() => {
-  const _board = [0,0,0,0,0,0,0,0,0];
+  const _board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   const getBoard = () => _board;
-  return { getBoard };
+  const applyMark = function (tileNum) {
+    _board[tileNum] = 1;
+  }
+  return { getBoard, applyMark };
 })();
 
 // object responsible for rendering board state to page
 const displayController = (() => {
   const _display = document.querySelector('#board');
-  const renderDisplay = function(board) {
-    board.forEach((tile) => { _createTile(tile); });
+  const renderDisplay = function (board) {
+    // clear current board
+    _display.replaceChildren();
+    // replace with new board
+    for (const [tileNum, tile] of board.entries()) {
+      _createTile(tileNum, tile);
+    }
   }
-  const _createTile = function() {
+  const _createTile = function (tileNum, tile) {
     const newTile = document.createElement('div');
     newTile.classList.add('tile');
+    newTile.addEventListener('click', function () { logicController.mark(tileNum) });
     _display.appendChild(newTile);
   }
   return { renderDisplay };
+})();
+
+// object responsible for controlling game logic
+const logicController = (() => {
+  const mark = function (tileNum) {
+    gameBoard.applyMark(tileNum);
+    displayController.renderDisplay(gameBoard.getBoard());
+  }
+  return { mark };
 })();
 
 displayController.renderDisplay(gameBoard.getBoard());
